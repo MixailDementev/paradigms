ff9a5bf0a0c6594cc825522bef65464842e81398
MDementev <vanneste1207@gmail.com>
2023-10-29 13:47:47 +0300

    task_1 from HW_6

---
+++ HW_6/task_1.py
@@ -0,0 +1,29 @@
+array = [3, 7, 12, 9, 25, 38, 43, 2, 99, 76, 33]
+target = int(input("Введите число для поиска: "))
+
+def binary_search(array, target):
+    low = 0
+    high = len(array) - 1
+    mid = 0
+
+
+    while low <= high:
+        mid = (low + high) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+array.sort()
+result = binary_search(array, target)
+print(array)
+
+if result > -1:
+    print("Индекс искомого элемента: ", result)
+else:
+    print("Искомый элемент не найден в массиве.")
+      
